9bb194911959cde77d4e6c7917033c6fa160ad74
Mads Brunn <mads@brunn.dk>
2013-09-24 22:10:44 +0200

    Added some typoscript

--- ts/page.ts
+++ ts/page.ts
@@ -1,4 +1,31 @@
 page = PAGE
 page{
 
+    headTag(
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> 
+    )
+
+
+    includeCSS{
+
+        normalize = {$template.path}css/normalize.min.css
+        main = {$template.path}css/main.css
+        screen = {$template.path}css/screen.css
+  
+    }
+  
+  
+    includeJS{
+
+        modernizr = {$template.path}js/vendor/modernizr-2.6.2.min.js    
+    
+    }
+  
+    
+    includeJSFooter{
+    
+        plugins = {$template.path}js/plugins.js
+        
+    }
 }
